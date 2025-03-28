e071e05ec08c32002cf4b7cf419292e3a160313a
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-03-28 08:17:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-
 import { createDirectories } from "./bin/createDirectories.js";
 import { createFiles } from "./bin/createFiles.js";
 import { initializeProject, installDependencies } from "./bin/initializer.js";
@@ -8,6 +7,7 @@ import { setupScripts } from "./bin/setupScripts.js";
 import chalk from "chalk";
 import path from "path";
 import { fileURLToPath } from "url";
+import inquirer from "inquirer";
 
 // Fix for __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -15,25 +15,14 @@ const __dirname = path.dirname(__filename);
 
 process.stdin.setEncoding("utf8");
 
-const createDirectories = require("./bin/createDirectories");
-const setupTests = require("./bin/setupTests");
-const createFiles = require("./bin/createFiles");
-const path = require('path');
-const scriptName = process.argv[1].split(path.sep).pop();
-const { initializeProject, installDependencies } = require("./bin/initializer");
-const setupScripts = require("./bin/setupScripts");
-const chalk = require("chalk");
-const inquirer = require("inquirer").default;
-
-
 // Remove test library flags from command args
-const args = process.argv.slice(2).filter(arg => !['--jest', '--mocha'].includes(arg));
+const args = process.argv.slice(2).filter(arg => !["--jest", "--mocha"].includes(arg));
 
 if (args.includes("--help")) {
   console.log("Express Project Generator");
   console.log("GitHub: https://github.com/developer-diganta/express-project-generator");
   console.log("");
-  console.log(`Usage: node ${scriptName} [options]`);
+  console.log(`Usage: node ${process.argv[1]} [options]`);
   console.log("Options:");
   console.log("  --help       Show this help message");
   console.log("  --version    Show version number");
@@ -54,92 +43,33 @@ if (args.includes("--version")) {
 }
 
 async function main() {
-    const responses = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'projectName',
-            message: 'Enter Project Name:',
-            default: 'my-app'
-        },
-        {
-            type: 'confirm',
-            name: 'addTests',
-            message: 'Would you like to add test scripts?',
-            default: false
-        },
-        {
-            type: 'list',
-            name: 'testFramework',
-            message: 'Which test framework would you like to use?',
-            choices: ['Jest', 'Mocha'],
-            when: (answers) => answers.addTests
-        }
-    ]);
-
-    const projectName = responses.projectName;
-    const testLibraries = {
-        jest: responses.testFramework === 'Jest',
-        mocha: responses.testFramework === 'Mocha'
-    };
-
-    // Calculate total steps based on test selection
-    const TEST_STEPS = [testLibraries.jest, testLibraries.mocha].filter(Boolean).length * 2;
-    const TOTAL_STEPS = 2 + 8 + 2 + TEST_STEPS + 1;
-
-
-console.log(chalk.blue("Enter Project Name: "));
-process.stdin.on("data", async function (data) {
-  projectName = data.trim();
-  let completedSteps = 0;
-  let lastPercentage = -1;
-
-  const updateProgress = () => {
-    completedSteps++;
-    const percentage = Math.round((completedSteps / 12) * 100);
-    if (percentage !== lastPercentage) {
-      process.stdout.write(chalk.blue(`[${percentage}%] `));
-      lastPercentage = percentage;
-    }
-  };
-
-  try {
-    await initializeProject(projectName, updateProgress);
-    await installDependencies(projectName, updateProgress);
-    await createDirectories(projectName, updateProgress);
-    await createFiles(projectName, updateProgress);
-    await setupScripts(projectName, updateProgress);
-    console.log(chalk.blue(`\n[100%] `) + chalk.green.bold("Project setup completed!"));
-  } catch (error) {
-    console.error(`Error generating project: ${error}`);
-    process.exit(1);
-  }
-});
-
+  console.log(chalk.blue("Enter Project Name: "));
+  process.stdin.on("data", async function (data) {
+    const projectName = data.trim();
     let completedSteps = 0;
     let lastPercentage = -1;
-    
+
     const updateProgress = () => {
-        completedSteps++;
-        const percentage = Math.round((completedSteps / TOTAL_STEPS) * 100);
-        if (percentage !== lastPercentage) {
-            process.stdout.write(chalk.blue(`[${percentage}%] `));
-            lastPercentage = percentage;
-        }    
+      completedSteps++;
+      const percentage = Math.round((completedSteps / 12) * 100);
+      if (percentage !== lastPercentage) {
+        process.stdout.write(chalk.blue(`[${percentage}%] `));
+        lastPercentage = percentage;
+      }
     };
 
     try {
-        await initializeProject(projectName, updateProgress);
-        await installDependencies(projectName, testLibraries);
-        await createDirectories(projectName, updateProgress);
-        await createFiles(projectName, updateProgress);
-        await setupScripts(projectName, testLibraries, updateProgress);
-        await setupTests(projectName, testLibraries, updateProgress);
-        console.log(chalk.blue(`\n[100%] `) + chalk.green.bold('Project setup completed!'));
+      await initializeProject(projectName, updateProgress);
+      await installDependencies(projectName, updateProgress);
+      await createDirectories(projectName, updateProgress);
+      await createFiles(projectName, updateProgress);
+      await setupScripts(projectName, updateProgress);
+      console.log(chalk.blue(`\n[100%] `) + chalk.green.bold("Project setup completed!"));
     } catch (error) {
-        console.error(`Error generating project: ${error}`);
-        process.exit(1);
+      console.error(`Error generating project: ${error}`);
+      process.exit(1);
     }
+  });
 }
 
 main();
-
